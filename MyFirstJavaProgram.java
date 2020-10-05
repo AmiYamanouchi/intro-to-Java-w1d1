66204c24a226cd33f5a58b738de72ff5db1b24ee
Ami Yamanouchi <amiyamanouchi@Amis-MacBook-Pro.local>
2020-10-05 10:58:53 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,17 @@
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        
+        byte myAge = 27;
+        System.out.println(myAge);
+
+        myAge = 30;
+
+        System.out.println(myAge);
+
+        String myName = "Ami Yamanouchi";
+        System.out.println(myName);
+
+        
+    }
+}
